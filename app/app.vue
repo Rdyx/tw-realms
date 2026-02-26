eb59fd0df066841e1cf6f2dfb2994cd09bce808d
Rdyx <tiranico.pro@gmail.com>
2026-02-26 18:33:58 +0100

    Add common settings for tailwind intellisense, add darkmode button

--- app/app.vue
+++ app/app.vue
@@ -1,8 +1,32 @@
+<script setup lang="ts">
+const colorMode = useColorMode();
+const theme = useState<string>("light");
+
+onMounted(() => {
+	theme.value = localStorage.getItem("theme") || colorMode.value;
+	colorMode.preference = theme.value;
+});
+
+const switchTheme = () => {
+	theme.value = localStorage.getItem("theme") === "light" ? "dark" : "light";
+	colorMode.preference = theme.value;
+	localStorage.setItem("theme", theme.value);
+};
+</script>
+
 <template>
-	<NuxtLayout>
+	<UApp>
 		<h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+		<UButton
+			size="sm"
+			color="primary"
+			variant="ghost"
+			:icon="theme === 'light' ? 'i-lucide-sun' : 'i-lucide-moon'"
+			@click="switchTheme"
+		/>
 
 		<NuxtRouteAnnouncer />
 		<NuxtPage />
-	</NuxtLayout>
+	</UApp>
 </template>
